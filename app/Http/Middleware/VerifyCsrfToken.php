40006d3d683cf84046010ab5fcdcf545921bc95d
Koki <wamauup.h.h.h@gmail.com>
2024-01-13 19:21:34 +0900

    CSRFバリデーションから登録画面とログイン画面を除外

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'https://professional-web-programming2-c3ecbc337e66.herokuapp.com/register/*',
+        'https://professional-web-programming2-c3ecbc337e66.herokuapp.com/login/*'
     ];
 }
